c7243e5d9c85c659c48d796bc77ba8aacd8e7b2b
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-05 13:46:51 +0500

    curd operations

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const {MongoClient, Db}= require('mongodb');
-const { getEnvironmentData } = require('worker_threads');
 let url = "mongodb://localhost:27017";
 let database = 'local';
 let client = new MongoClient(url);
@@ -12,10 +11,18 @@ async function getData()
     return db.collection('startup_log');
 }
 // curd operations
-// getData().then((response)=>{
-//    response.find({}).toArray().then((data)=>{
-//     console.log(data);
-//    });
-// })
 
+/*
+    getData().then((response)=>{
+    response.find({}).toArray().then((data)=>{
+    console.log(data);
+    });
+}) */
+
+let main = async () => {
+    let data = await getData();
+    data = await data.find().toArray();
+    console.log(data);
+}
+main();
 
