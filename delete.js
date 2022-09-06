03c488454ec5601e844d25f9dc0de2f1f8a648cf
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-06 16:23:49 +0500

    crud operation in api

--- delete.js
+++ delete.js
@@ -4,7 +4,7 @@ const getData = require('./mongodb');
 
 const deleteData =  async () => {
     const data = await getData();
-    const result = await data.deleteOne({ name:'Windows'});
+    const result = await data.delete({name:"Clementina DuBuque"});
 
     if(result.acknowledged){
         console.warn("data deleted");
